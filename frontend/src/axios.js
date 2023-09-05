52ef19bed6673e2b9f6a01f49e419533d54e0edc
Atishey jain <atisheyjain1232@gmail.com>
2023-09-06 01:48:09 +0530

    update front build files and change BE api base url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 // console.log(Cookies.get());
 const axios = Axios.create({
   // baseURL: "http://localhost:8000/api/v1/",
-  baseURL: "https://www.inqueue.co.in/api/v1/",
+  baseURL: "https://inqueue1.onrender.com/api/v1/",
   // baseURL: "https://sleepy-temple-99502.herokuapp.com/api/v1/",
   // timeout: 10000,
   // withCredentials: true,
